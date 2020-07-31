c070adc9b5255a59cc98b7668682b879a967ae2d
GesusKryst <gec68@nau.edu>
2020-07-31 16:09:32 -0700

    Added universal navigation bar

---
+++ js/navigation.js
@@ -0,0 +1,13 @@
+/*
+    Here is a super ugly looking way to 
+    create a navigation bar that I can reference across
+    all new pages
+*/
+document.write('<ul>\
+        <li>Navigation</li><hr>\
+        <li><a href="games.html">Games</a></li>\
+        <li><a href="gametools.html">Game Tools</a></li> \
+        <li><a href="visualSort.html">Visuals</a></li> \
+        <li><a href="ml_demo.html">Machine Learning</a></li> \
+        <li><a href="https://github.com/GesusKryst?tab=repositories">My GitHub</a></li> \
+    </ul>');
