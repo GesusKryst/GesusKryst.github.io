4c81efc2e9d267e81bc57335fb10295c0d39757e
unknown <gec68@nau.edu>
2020-09-11 23:51:55 -0700

    Updated Styles

--- js/navigation.js
+++ js/navigation.js
@@ -4,9 +4,9 @@
     all new pages
 */
 document.write('<ul>\
-        <li>Navigation</li><hr>\
+        <li id= anime>Navigation</li><hr>\
         <li><a href="games.html">Games</a></li>\
-        <li><a href="gametools.html">Game Tools</a></li> \
+        <li><a href="tools.html">Tools</a></li> \
         <li><a href="visualSort.html">Visuals</a></li> \
         <li><a href="ml_demo.html">Machine Learning</a></li> \
         <li><a href="https://github.com/GesusKryst?tab=repositories">My GitHub</a></li> \
