064d0ee6835665cba02ea2b17db74ef21b123991
GesusKryst <gec68@nau.edu>
2020-10-03 11:42:43 -0700

    Finally, a website

--- js/sorting.js
+++
@@ -1,5 +0,0 @@
-function openHelp()
-{
-    window.open("https://p5js.org/get-started/")
-}
-
--- v3/js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("body").hide().show(500);
-    $("header").load("templates/header.html");
-    $("nav").load("templates/navbar.html");
-    $("footer").load("templates/footer.html");
-
-});
