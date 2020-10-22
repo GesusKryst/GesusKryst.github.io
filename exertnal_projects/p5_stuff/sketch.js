15660b80320d560181145d6870b145378ef034fc
unknown <gec68@nau.edu>
2020-10-22 06:16:47 -0700

    Pushing content to work on out of town

--- exertnal_projects/p5_stuff/sketch.js
+++ exertnal_projects/p5_stuff/sketch.js
@@ -1,20 +1,30 @@
-    // Create an array of values
-    var totalValues = 100;
-    var maxValue = 10;
-    var unsortedList = [];
-    var iter1 = 0;
-    var iter2 = 0;
-    for (let i = 0; i < totalValues; i++)
-    {
+function createNode()
+{}
+
+
+/*
+// Create an array of values
+var totalValues = 100;
+var maxValue = 10;
+var unsortedList = [];
+var iter1 = 0;
+var iter2 = 0;
+
+function createArray(arrayLength){
+    let anArray = [];
+    for (let i = 0; i < arrayLength; i++) {
         let val = 0;
-        while(val == 0)
-        {
+        while (val == 0) {
             val = Math.round(Math.random() * maxValue);
         }
-        unsortedList.push(val);
+        anArray.push(val);
     }
+    return anArray;
+}
 
-function setup(){
+unsortedList = createArray(totalValues);
+
+function setup() {
     // Canvas Size Setup
     var width = 800;
     var height = 500;
@@ -24,27 +34,26 @@ function setup(){
 
 
 
-   // Draw values
-   console.log("Unsorted Values: " + unsortedList);
-   
+    // Draw values
+    console.log("Unsorted Values: " + unsortedList);
+
 }
 
-function draw(){
+function draw() {
     // Functions
-    function drawArr(){
-        setTimeout(() => {}, 2000);
+    function drawArr() {
+        setTimeout(() => { }, 2000);
         background(255);
-        for(let i = 0; i <= unsortedList.length; i++)
-        {
+        for (let i = 0; i <= unsortedList.length; i++) {
             fill(155, 155, 200);
-            rect((i * width / totalValues), height,  (width / totalValues) - 1, -(unsortedList[i] * height / 10));
+            rect((i * width / totalValues), height, (width / totalValues) - 1, -(unsortedList[i] * height / 10));
         }
     }
 
-    function sortArr(){
+    function sortArr() {
         iter1 = 0;
-        while(iter1 <= unsortedList.length - 1){
-            if(unsortedList[iter1] < unsortedList[iter2]){
+        while (iter1 <= unsortedList.length - 1) {
+            if (unsortedList[iter1] < unsortedList[iter2]) {
                 let temp = unsortedList[iter1];
                 unsortedList[iter1] = unsortedList[iter2];
                 unsortedList[iter2] = temp;
@@ -59,9 +68,10 @@ function draw(){
     // Run Program
     drawArr();
     sortArr();
-
-    if(iter2 == unsortedList.length){
-        noLoop();
+    if (iter2 == unsortedList.length) {
+        iter1, iter2 = 0;
+        unsortedList = createArray(Math.random() * 100);
+        setTimeout(()=>{}, 5000);
     }
 }
-
+*/
